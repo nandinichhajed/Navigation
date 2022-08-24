eea4683df79f1bbd10380e7ff241201168d516fa
Nandini Chhajed <nandinichhajed08@gmail.com>
2022-08-24 19:48:44 +0530

    creating mytables model

--- server/navigation/api/models.py
+++ server/navigation/api/models.py
@@ -5,9 +5,9 @@ from django.db import models
 
 class Mytable(models.Model):
     ID = models.IntegerField(db_column="ID") #name of column of existing db
-    Course_Over_Ground = models.CharField(db_column="Course_Over_Ground")
-    Latitude = models.CharField(db_column="Latitude")
-    Longitude = models.CharField(db_column="Longitude")
+    Course_Over_Ground = models.CharField(db_column="Course_Over_Ground", max_length=200)
+    Latitude = models.CharField(db_column="Latitude", max_length=200)
+    Longitude = models.CharField(db_column="Longitude", max_length=200)
     Ship_ID = models.IntegerField(db_column="Ship_ID")
     Speed = models.IntegerField(db_column="Speed")
     Time = models.TimeField(db_column="Time")
