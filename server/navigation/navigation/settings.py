632cb0f56a269bf27ee20f0397eecca1287c803f
Nandini Chhajed <nandinichhajed08@gmail.com>
2022-08-24 22:29:18 +0530

    depoly0.1

--- server/navigation/navigation/settings.py
+++ server/navigation/navigation/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
+from decouple import config
+import django_heroku
+# django_heroku.settings(locals())
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ug87p9i-d*%e#at@&5ic00ijr^$h_2gzrk94m+mggm18c9^c@$'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
