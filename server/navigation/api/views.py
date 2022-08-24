95b968354b9a23d605d2ba367e7ce885ba55c3a0
Nandini Chhajed <nandinichhajed08@gmail.com>
2022-08-24 19:51:10 +0530

    creating mytable views

--- server/navigation/api/views.py
+++ server/navigation/api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from rest_framework import viewsets
+from .serializers import MytableSerializer
+from .models import Mytable
 # Create your views here.
+
+class MytableViewSet(viewsets.ModelViewSet):
+    queryset = Mytable.objects.all().order_by('ID')[:10]
+    serializer_class = MytableSerializer
