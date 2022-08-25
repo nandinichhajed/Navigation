199bd5be44e8d0e85be32a7b24816f2a27417f0d
Nandini Chhajed <nandinichhajed08@gmail.com>
2022-08-25 12:34:41 +0530

    creating models

--- navig/api/models.py
+++ navig/api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Mytable(models.Model):
+    id = models.IntegerField(db_column="id", primary_key=True) #name of column of existing db
+    Course_Over_Ground = models.CharField(db_column="Course_Over_Ground", max_length=200)
+    Latitude = models.CharField(db_column="Latitude", max_length=200)
+    Longitude = models.CharField(db_column="Longitude", max_length=200)
+    Ship_ID = models.IntegerField(db_column="Ship_ID")
+    Speed = models.IntegerField(db_column="Speed")
+    Time = models.TimeField(db_column="Time")
+    
+    class Meta:
+        db_table = "mytable"
+    
+    
+    def __str__(self):
+        return self.id
