682e4d656a6cb55e6868b9aab4b07f2fce5c3968
Nandini Chhajed <nandinichhajed08@gmail.com>
2022-08-25 10:57:07 +0530

    restarting

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn navigation.wsgi --log-file -
--- navigation/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Mytable
-
-class MytableSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Mytable
-        fields = ('ID','Course_Over_Ground', 'Latitude', 'Longitude', 'Ship_ID', 'Speed', 'Time')
