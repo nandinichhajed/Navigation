6df8e3378604161d9c42cf985621ef665722e089
Nandini Chhajed <nandinichhajed08@gmail.com>
2022-08-27 18:20:03 +0530

    removing deployement changes

--- navig/navig/settings.py
+++ navig/navig/settings.py
@@ -9,8 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-import os
-from decouple import config
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,18 +20,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-$1(u-oc#3ljpel$g+tc&)v)%q%jlu@)g%-%&rmqcsnln1ia0a^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', 'navig.azurewebsites.net']
-
+ALLOWED_HOSTS = []
 
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,7 +42,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -85,37 +81,16 @@ WSGI_APPLICATION = 'navig.wsgi.application'
 #     }
 # }
 
-import dj_database_url
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'nrsc',
-#         'USER': 'postgres',
-#         'PASSWORD': 'nandinichhajed',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'nrsc',
-        'USER': 'nandini',
-        'PASSWORD': 'Nandinic08@',
-        'HOST': 'nrsc.postgres.database.azure.com',
+        'USER': 'postgres',
+        'PASSWORD': 'nandinichhajed',
+        'HOST': '127.0.0.1',
         'PORT': '5432',
-        'OPTIONS': {
-            "sslmode": "require"
-        },
     }
 }
-
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
-WHITENOISE_USE_FINDERS = True
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
