cafade9cc874124541128c5a13ae212476e1238a
Nandini Chhajed <nandinichhajed08@gmail.com>
2022-08-28 10:17:44 +0530

    creating detailed view of ship data

--- navig/api/urls.py
+++ navig/api/urls.py
@@ -2,9 +2,11 @@ from django.urls import path, include
 from rest_framework import routers
 from . import views
 
-router = routers.DefaultRouter()
-router.register(r'', views.MytableViewSet)
+# router = routers.DefaultRouter()
+# router.register(r'', views.MytableViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    # path('', include(router.urls)),
+    path('v1/ships/', views.table, name="table"),
+    path('v1/ships/<int:id>/', views.table_detail, name="table_detail"),
 ]
--- navig/api/views.py
+++ navig/api/views.py
@@ -1,9 +1,18 @@
 from rest_framework import viewsets
 from .serializers import MytableSerializer
 from .models import Mytable
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 # Create your views here.
 
-class MytableViewSet(viewsets.ModelViewSet):
-    queryset = Mytable.objects.all().order_by('id')[:10]
-    serializer_class = MytableSerializer
+@api_view(['GET'])
+def table(request):
+	queryset = Mytable.objects.all().order_by('id')[:10]
+	serializer = MytableSerializer(queryset, many=True)
+	return Response(serializer.data)
+
+@api_view(['GET'])
+def table_detail(request, id):
+    table = Mytable.objects.get(pk=id)
+    serializer = MytableSerializer(table, many=False)
+    return Response(serializer.data)
